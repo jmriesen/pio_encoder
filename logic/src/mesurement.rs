e765e6af63d47f9a03573ee3604d8d23b0eb19fe
jmriesen <jacobriesen@gmail.com>
2025-08-22 14:33:00 -0500

    Adding unit tests for the speed_bounds

--- logic/src/mesurement.rs
+++ logic/src/mesurement.rs
@@ -47,33 +47,40 @@ fn bound_speed(
     current: Mesurement,
     cali: &CalibrationData,
 ) -> Speed {
+    let (speed_lower_bound, speed_upper_bound) = calculate_speed_bounds(previuse, current, cali);
+    speed.clamp(speed_lower_bound, speed_upper_bound)
+}
+
+/// Calculate the lower and upper speed bounds giving the current and previuse measurements
+fn calculate_speed_bounds(
+    previuse: Mesurement,
+    current: Mesurement,
+    cali: &[u8; 4],
+) -> (Speed, Speed) {
     let mesured_position = current.mesured_position(cali);
     let first_mesurement_in_step = current.step_instant > previuse.sample_instant;
 
     let time_since_last_sample = current.step_instant - previuse.sample_instant;
     let time_since_current_sample = current.sample_instant - current.step_instant;
     let previuse_sample_is_farther = time_since_last_sample > time_since_current_sample;
-    let (speed_lower_bound,speed_upper_bound) =
     //If this is the first measurement we want to use whichever measurement is **Farther away** for
     //our estimates.
     //Using the longer delta time gives less uncertainty in our estimates
-        if first_mesurement_in_step && previuse_sample_is_farther {
-            let (lower_bound, upper_bound) = previuse.steps.bounds(cali);
-        //NOTE: this is (initial - final) rather than (final-initial) to compensate for the fact 
+    if first_mesurement_in_step && previuse_sample_is_farther {
+        let (lower_bound, upper_bound) = previuse.steps.bounds(cali);
+        //NOTE: this is (initial - final) rather than (final-initial) to compensate for the fact
         //that we don't have negative durations.
-            (
-                Speed::new(mesured_position - upper_bound, time_since_last_sample),
-                Speed::new(mesured_position - lower_bound, time_since_last_sample),
-            )
-        } else {
-            let (lower_bound, upper_bound) = current.steps.bounds(cali);
-            (
-                Speed::new(lower_bound - mesured_position, time_since_current_sample),
-                Speed::new(upper_bound - mesured_position, time_since_current_sample),
-            )
-        };
-
-    speed.clamp(speed_lower_bound, speed_upper_bound)
+        (
+            Speed::new(mesured_position - upper_bound, time_since_last_sample),
+            Speed::new(mesured_position - lower_bound, time_since_last_sample),
+        )
+    } else {
+        let (lower_bound, upper_bound) = current.steps.bounds(cali);
+        (
+            Speed::new(lower_bound - mesured_position, time_since_current_sample),
+            Speed::new(upper_bound - mesured_position, time_since_current_sample),
+        )
+    }
 }
 #[cfg(test)]
 mod tests {
@@ -83,7 +90,7 @@ mod tests {
     use embassy_time::Duration;
 
     #[test]
-    fn mesurment() {
+    fn construct_mesurement_from_data() {
         let time = Instant::from_secs(1);
         assert_eq!(
             Mesurement::new(DirectionDuration(0 - 50), Step::new(42), time, 10),
@@ -139,6 +146,64 @@ mod tests {
         assert_eq!(
             speed,
             Speed::new(SubStep::new(64), Duration::from_millis(20))
+        );
+    }
+    #[test]
+    fn last_smaple_time_is_further_away_from_step_time() {
+        let delta = Duration::from_millis(10);
+        let last_known_position_time = Instant::from_millis(30);
+        let speed = calculate_speed_bounds(
+            Mesurement {
+                steps: Step::new(0),
+                direction: Direction::Clockwise,
+                //NOTE: This step time dose not matter
+                step_instant: Instant::from_millis(0),
+                sample_instant: last_known_position_time - delta,
+            },
+            Mesurement {
+                steps: Step::new(10),
+                direction: Direction::Clockwise,
+                //NOTE: This is the step time we care about.
+                step_instant: last_known_position_time,
+                sample_instant: last_known_position_time + delta / 2,
+            },
+            &EQUAL_STEPS,
+        );
+        assert_eq!(
+            speed,
+            (
+                Speed::new(SubStep::new(64 * 9), delta),
+                Speed::new(SubStep::new(64 * 10), delta)
+            )
+        );
+    }
+    #[test]
+    fn current_smaple_time_is_further_away_from_step_time() {
+        //Since the larger time windows is withing the step,
+        //we completely ignore the previous measurement
+        let delta = Duration::from_millis(10);
+        let last_known_position_time = Instant::from_millis(30);
+        let speed = calculate_speed_bounds(
+            Mesurement {
+                steps: Step::new(0),
+                direction: Direction::Clockwise,
+                step_instant: Instant::from_millis(0),
+                sample_instant: last_known_position_time - delta / 2,
+            },
+            Mesurement {
+                steps: Step::new(10),
+                direction: Direction::Clockwise,
+                step_instant: last_known_position_time,
+                sample_instant: last_known_position_time + delta,
+            },
+            &EQUAL_STEPS,
+        );
+        assert_eq!(
+            speed,
+            (
+                Speed::new(SubStep::new(0), delta),
+                Speed::new(SubStep::new(64), delta)
+            )
         );
     }
 }
